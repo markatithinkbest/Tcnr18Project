d530aa2c01c7529ef26363f53518e7550f16551b
Mark Chen <mark@ithinkbest.com>
2015-04-13 13:27:22 +0800

    000

---
+++ src/tcnr18/com/project/MainActivity.java
@@ -0,0 +1,44 @@
+package tcnr18.com.project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	  public void onClickLaunch(View view){
+//   	   Intent intent = new Intent(this, MainActivity.class);
+   	   Intent intent = new Intent(this, NavigationDrawerActivity.class);
+
+       startActivity(intent);
+       finish();
+
+   }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+		return super.onOptionsItemSelected(item);
+	}
+}
